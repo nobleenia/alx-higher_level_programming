fa15e23a648d6c15e917183752160a7c58c9468d
Noble <neluwah@gmail.com>
2023-09-05 08:41:06 -0700

    Update: 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for first in range(9):
     for second in range(10):
         if first < second:
             if first == 8 and second == 9:
-                print("{}{}".format(first, second), end="")
+                print("{}{}".format(first, second))
             else:
                 print("{}{}".format(first, second), end=", ")
