fb8ed76027e8b5580d88faa6e89154fece01d8ad
Noble <neluwah@gmail.com>
2023-09-07 10:12:35 -0700

    Corrections: 102-magic_calculation.py & 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,18 +2,25 @@
 from calculator_1 import add, sub, mul, div
 from sys import argv
 
+argv_len = len(argv)
+operands = ["+", "-", "*", "/"]
+operator = argv[2]
+
 if __name__ == "__main__":
     if len(argv) != 4:
-        print("Usage:", argv[0], "<a> <operator> <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
-    operator = argv[2]
-    operands = {"+": add, "-": sub, "*": mul, "/": div}
-
-    if operator not in operands:
+    elif operator  not in operands:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-
-    a = int(argv[1])
-    b = int(argv[3])
-    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, operands[operator](a, b)))
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+        if operator == "+":
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        elif operator == "-":
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif operator == "*":
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        else:
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
