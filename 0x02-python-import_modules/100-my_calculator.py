5c7fdff986cbc99ce6440827885952cffbd63517
Noble <neluwah@gmail.com>
2023-09-07 10:30:32 -0700

    Corrections

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 from sys import argv
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
     argv_len = len(argv)
