67a9b9c2edcb58850f48e9709f9670c47f303739
Noble <neluwah@gmail.com>
2023-09-07 08:50:58 -0700

    Corrections

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,4 +4,4 @@ a = 1
 b = 2
 
 if __name__ == "__main__":
-    print(f"{a:d} + {b:d} = {add(a, b)}", )
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ a = 10
 b = 5
 
 if __name__ == "__main__":
-    print(f"{a:d} + {b:d} = {add(a, b)}")
-    print(f"{a:d} - {b:d} = {sub(a, b)}")
-    print(f"{a:d} * {b:d} = {mul(a, b)}")
-    print(f"{a:d} / {b:d} = {div(a, b)}")
+    print(f"{a:d} + {b:d} = {add(a, b):d}")
+    print(f"{a:d} - {b:d} = {sub(a, b):d}")
+    print(f"{a:d} * {b:d} = {mul(a, b):d}")
+    print(f"{a:d} / {b:d} = {div(a, b):d}")
