416e3cc70e50cbf8aa71fcf59c19cce7f770b126
Noble <neluwah@gmail.com>
2023-09-07 08:56:23 -0700

    Correction: 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ a = 10
 b = 5
 
 if __name__ == "__main__":
-    print(f"{a:d} + {b:d} = {add(a, b):d}")
-    print(f"{a:d} - {b:d} = {sub(a, b):d}")
-    print(f"{a:d} * {b:d} = {mul(a, b):d}")
-    print(f"{a:d} / {b:d} = {div(a, b):d}")
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b))
