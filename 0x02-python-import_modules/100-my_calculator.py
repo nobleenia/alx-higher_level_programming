f24c0cac0c628d16dfd7bea71c7e1bcb90ee423e
Noble <neluwah@gmail.com>
2023-09-07 09:57:02 -0700

    Corrections: Advanced Tasks

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ from sys import argv
 
 if __name__ == "__main__":
     if len(argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Usage:", argv[0], "<a> <operator> <b>")
         exit(1)
 
     operator = argv[2]
