3c69d272d15cd685620c980a27f6afc5a96de5a0
Noble <neluwah@gmail.com>
2023-09-12 10:48:54 -0700

    Update: Corrections

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     list_cpy = my_list.copy()
 
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return list_cpy
     list_cpy[idx] = element
     return list_cpy
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     item_1 = len(sentence)
-    item_2 = sentence[0]
+    if item_1 == 0:
+        item_2 = None
+    else:
+        item_2 = sentence[0]
 
     return (item_1, item_2)
