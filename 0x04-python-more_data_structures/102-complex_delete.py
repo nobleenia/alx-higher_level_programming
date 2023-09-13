bda881524328b9b575fb4df07e21d2637b7e88c5
Noble <neluwah@gmail.com>
2023-09-13 11:43:10 -0700

    102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,4 +3,4 @@ def complex_delete(a_dictionary, value):
     for item in list(a_dictionary.keys()):
         if a_dictionary[item] == value:
            del a_dictionary[item]
-        return a_dictionary
+    return a_dictionary
