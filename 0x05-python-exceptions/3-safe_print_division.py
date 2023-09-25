d6bbee2d4692637c8c135388bdd202eb45d4c672
Noble <neluwah@gmail.com>
2023-09-25 13:29:58 -0700

    Task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    div = None
+    quotient = None
     try:
-        div = a / b
+        quotient = a / b
     except (ZeroDivisionError, TypeError):
         pass
     finally:
-        print("Inside result : {}".format(div))
-    return div
+        print("Inside result : {}".format(quotient))
+        return quotient
