263d3beddb84073a19b02e850b525982dfcbf1f5
Noble <neluwah@gmail.com>
2023-12-11 08:59:29 -0800

    Update: 11-second_biggest.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,7 +4,7 @@ function factorial (n) {
   if (isNaN(n) || n === 0) {
     return 1;
   } else {
-      return n * factorial(n - 1);
+    return n * factorial(n - 1);
   }
 }
 
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,7 +3,7 @@
 const myVar1 = parseInt(process.argv[2]);
 const myVar2 = parseInt(process.argv[3]);
 
-function add (a, b) {
+function add(a, b) {
     if (isNaN(a) || isNaN(b)) {
       return NaN;
   }
