b0f1dccb7b65a5efb582db816f9a75fc955c338e
Noble <neluwah@gmail.com>
2023-12-11 07:34:19 -0800

    Update: 7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const number = parseInt(process.argv[2]);
+
+if (isNan(number) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < number; i++) {
+    console.log('C is fun');
+  }
+}
