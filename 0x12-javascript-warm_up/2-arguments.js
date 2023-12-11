69a06a8574a7e3b32dff3242e99e385d6eec9c55
Noble <neluwah@gmail.com>
2023-12-11 05:54:55 -0800

    Update: Tasks 1 & 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (process.argv.Lenght <= 2){
+    console.log('No argument');
+} else if (process.argv.Length = 3){
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}
