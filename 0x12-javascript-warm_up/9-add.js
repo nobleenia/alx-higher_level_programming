09046332b76bb13e5b71b1784abaaae8836a0a7c
Noble <neluwah@gmail.com>
2023-12-11 08:24:41 -0800

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,11 +4,10 @@ const myVar1 = parseInt(process.argv[2]);
 const myVar2 = parseInt(process.argv[3]);
 
 function add(a, b) {
-  if (isNaN(a) || isNaN(b)) {
-    return NaN;
+    if (isNaN(a) || isNaN(b)) {
+	return NaN;
   }
   return a + b;
 }
-
 
 console.log(add(myVar1, myVar2));
