f4dd23bc8e23d89d949251eacc64aa49e284c725
Noble <neluwah@gmail.com>
2023-12-11 08:13:00 -0800

    Update: 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 
-const myVar1 = process.argv[2];
-const myVar2 = process.argv[3];
-
 function add(a, b) {
-  console.log(a + b);
+    let sum = a + b;
+    console.log(a, b, sum);
 }
-
-add(myVar1, myVar2);
