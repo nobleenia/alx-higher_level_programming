591b1ac14b1f99bbd386ac4d55d6fded80e6f45f
Noble <neluwah@gmail.com>
2023-12-11 06:42:54 -0800

    Update: 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-if (process.argv.length === 0) {
-    console.log('No argument');
-} else {
+if (process.argv.length[2]) {
     console.log(process.argv[2]);
+} else {
+    console.log('No argument');
 }
