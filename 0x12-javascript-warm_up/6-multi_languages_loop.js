e9368ba9a0fa87787cb6d5afb832ba6caad4879a
Noble <neluwah@gmail.com>
2023-12-11 07:23:33 -0800

    Update: 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
-const lines = ["C is fun",
-	       "Python is cool",
-	       "JavaScript is amazing"];
+const lines = [
+  "C is fun",
+  "Python is cool",
+    "JavaScript is amazing"
+];
 
 lines.forEach(line => console.log(line));
