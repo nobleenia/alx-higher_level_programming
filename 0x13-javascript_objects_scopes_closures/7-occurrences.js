e07f5413c549469c625dcf803a42904403707213
Noble <neluwah@gmail.com>
2023-12-12 13:57:28 -0800

    Corrections

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-  const count = 0;
-  for (const i in list) {
+  let count = 0;
+  for (let i in list) {
     if (list[i] === searchElement){
       count++;
     }
