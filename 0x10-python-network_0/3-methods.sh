96d52761d496211543acd1228adb604dbb807d17
Noble <neluwah@gmail.com>
2024-01-05 09:06:03 -0800

    Update: Basic Tasks

---
+++ 0x10-python-network_0/3-methods.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Takes in a URL and displays all HTTP methods the server will accept
+curl -sI "$1" | grep -i "Allow" | awk -F ": " '{ print $2 }'
---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module finds a peak in a list of unsorted integers
+"""
+
+def find_peak(list_of_integers):
+    """
+    Finds a peak in a list of unsorted integers
+    """
+    if bool(list_of_integers) is False:
+        return None
+    list_of_integers.sort()
+    peak_int = list_of_integers[-1]
+
+    return peak_int
