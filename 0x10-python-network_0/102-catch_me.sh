1e9f8dc7db831f0712778b6106ea86d79bb17eee
Noble <neluwah@gmail.com>
2024-01-05 12:06:56 -0800

    Update: 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # cript that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
-curl -s -L -X PUT -d user_id=98 -H "Origin: ALXSchool" 0.0.0.0:5000/catch_me
+curl -sLX PUT -d user_id=98 -H "Origin: ALXSchool" 0.0.0.0:5000/catch_me
