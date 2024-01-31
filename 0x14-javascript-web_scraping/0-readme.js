933b132aee6cd0a93e01858a0a5f7551cde1c4ff
Noble <neluwah@gmail.com>
2024-01-31 01:13:06 -0800

    Update: 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,5 +4,5 @@ const fs = require('fs');
 const fileName = process.argv[2];
 
 fs.readFile(fileName, 'utf8', function (error, content) {
-    console.log(error || content);
+  console.log(error || content);
 });
